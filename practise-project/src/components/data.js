4febce14f1bb8fb5358d7044cc069c55909e15b4
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-02-21 02:13:00 +0500

    first commit

---
+++ practise-project/src/components/data.js
@@ -0,0 +1,6 @@
+var data = {
+    description :"Goku, the protagonist of the *Dragon Ball* series, is a Saiyan warrior known for his boundless strength, determination, and kind heart. Originally sent to Earth as a baby, he grows up to become Earth's greatest protector, constantly pushing his limits and surpassing his own power. Goku's iconic transformations, such as Super Saiyan and Ultra Instinct, make him one of the strongest fighters in the universe. He is always driven by the desire to become stronger and to protect his friends and loved ones, embodying the spirit of never giving up in the face of adversity.",
+     heading1 : "SUPER SAIYAN",
+     heading2 : "GOKU",
+}
+export default data;
