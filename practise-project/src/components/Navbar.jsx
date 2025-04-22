70b19df38975392331dc373b852f6f3e8143b734
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-04-22 23:04:11 +0500

    .

--- practise-project/src/components/Navbar.jsx
+++ practise-project/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <>
       <nav>
         <ul className="flex justify-around bg-gray-800 text-white p-4 cursor-pointer">
-          <li> <link to={"/"} >Home</link></li>
+          <li><link to={"/"} >Home</link></li>
           <li><link to={""} >About</link></li>
           <li><link to={""} >Contacts</link></li>
           <li><link to={""} >Others</link></li>
