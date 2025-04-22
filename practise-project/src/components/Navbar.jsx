6a6b4d79a6b4f4391dbbdee795c8a818fb91d3c3
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-04-22 23:03:34 +0500

    .

--- practise-project/src/components/Navbar.jsx
+++ practise-project/src/components/Navbar.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 
 const Navbar = () => {
-    return(
-        <>
-          <nav>
-            <ul className="flex justify-around bg-gray-800 text-white p-4 cursor-pointer">
-              <li> <link to={"/"} >Home</link></li>
-              <li><link to={""} >About</link></li>
-              <li><link to={""} >Contacts</link></li>
-              <li><link to={""} >Others</link></li>
-            </ul>
-          </nav>
+  return (
+    <>
+      <nav>
+        <ul className="flex justify-around bg-gray-800 text-white p-4 cursor-pointer">
+          <li> <link to={"/"} >Home</link></li>
+          <li><link to={""} >About</link></li>
+          <li><link to={""} >Contacts</link></li>
+          <li><link to={""} >Others</link></li>
+        </ul>
+      </nav>
 
-      </>
-    )
+    </>
+  )
 }
 export default Navbar;
